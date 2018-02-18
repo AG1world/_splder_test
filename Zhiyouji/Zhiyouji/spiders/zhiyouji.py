b34a86f7f8fef4eb70744dad2353b254bf245d24
andrew.gao <411956433@qq.com>
2018-02-18 23:39:31 +0800

    职友集获取完成,待修settings配置

--- Zhiyouji/Zhiyouji/spiders/zhiyouji.py
+++ Zhiyouji/Zhiyouji/spiders/zhiyouji.py
@@ -1,45 +1,79 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import time
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-from items import ZhiyoujiItem
+from Zhiyouji.items import ZhiyoujiItem
 
-class ZhiyoujiSpider(CrawlSpider):
+# 1. 导入类
+from scrapy_redis.spiders import RedisCrawlSpider
+# 2. 修改类的继承
+
+class ZhiyoujiSpider(RedisCrawlSpider):
+# class ZhiyoujiSpider(CrawlSpider):
     name = 'zhiyouji'
-    allowed_domains = ['jobui.com']
-    start_urls = ['http://']
+# 3. 注销允许的域和url
+    # allowed_domains = ['jobui.com']
+    # start_urls = ['http://www.jobui.com/cmp?area=%E5%85%A8%E5%9B%BD']
+# 4. redis_key
+    redis_key = 'zhiyouji'
+# 5. 编写init,动态获取允许的域名
+    def __init__(self, *args, **kwargs):
+        domain = kwargs.pop('domain', '')
+        self.allowed_domains = list(filter(None, domain.split(',')))
+        super(ZhiyoujiSpider, self).__init__(*args, **kwargs)
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-        # Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'www.jobui.com/cmp\?area=%E5%85%A8%E5%9B%BD/'), callback='parse_item', follow=True),
+
+        Rule(LinkExtractor(allow=r'/company/\d+/$'), callback='parse_item', follow=True),
+
     )
 
     def parse_item(self, response):
         '解析目标企业信息'
+        # print(response.url)
         item = ZhiyoujiItem()
 
-        data_source = scrapy.Field()
+        item['data_source'] = response.url
 
-        timestamp = scrapy.Field()
+        item['timestamp'] = time.time()
 
-        company_name = scrapy.Field()
+        item['company_name'] = response.xpath('//*[@id="companyH1"]/@data-companyname').extract_first()
+        # 浏览次数
+        item['views'] = response.xpath('/html/body/div[2]/div[1]/div/div[1]/div/div[2]/div[1]/text()').extract_first()
+        if item['views'] :
+            item['views'] = item['views'].split('人')[0].strip()
 
-        views = scrapy.Field()
+        item['slogan'] = response.xpath('//*[@id="companyH1"]/@data-companyname').extract_first()
 
-        slogan = scrapy.Field()
+        item['category'] = response.xpath('/html/body/div[2]/div[1]/div/div[1]/div[2]/p/text()').extract_first()
 
-        category = scrapy.Field()
+        item['industry'] = response.xpath('//*[@id="cmp-intro"]/div/div[2]/dl/dd[2]/a/text()').extract_first()
 
-        industry = scrapy.Field()
+        item['desc'] = response.xpath('//*[@id="textShowMore"]/text()').extract_first()
 
-        desc = scrapy.Field()
+        item['praise'] = response.xpath('//div[@class="swf-contA"]/div/h3/text()').extract_first()
 
-        praise = scrapy.Field()
+        item['salary'] = response.xpath('/html/body/div[3]/div[1]/div[1]/div[3]/div[2]/div[1]/h3/text()').extract_first()
 
-        salary = scrapy.Field()
+        item['address'] = response.xpath('//*[@class="dlli fs16"]/dd[1]/text()').extract_first()
 
-        finance_info = scrapy.Field()
+        item['contact'] = response.xpath('//*[@class="j-shower1 dn"]/dd/text()').extract_first()
+        if item['contact'] :
+            item['contact'] = item['contact'].split('/')[0].strip()
 
-        address = scrapy.Field()
-
-        contact = scrapy.Field()
+        finance_info = []
+        node_list = response.xpath('/html/body/div[3]/div[1]/div[1]/div[6]/ul/li')
+        print('---------',node_list)
+        # 遍历节点信息
+        for node in node_list:
+            temp = {}
+            temp['time'] = node.xpath('./span[1]/text()').extract_first()
+            temp['status'] = node.xpath('./h3/text()').extract_first()
+            temp['sum'] = node.xpath('./span[2]/text()').extract_first()
+            temp['investor'] = node.xpath('./span[3]/text()').extract_first()
+            finance_info.append(temp)
+        item['finance_info'] = finance_info
+        print(item)
+        # yield item
